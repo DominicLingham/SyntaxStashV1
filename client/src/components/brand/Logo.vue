6b8d63804f28f83266bb7f985c6cf5885f71f3b9
Dominic Lingham <dominic.lingham@educationcompany.co.uk>
2025-03-03 17:13:43 +0000

    feat: amend logo coloring

--- client/src/components/brand/Logo.vue
+++ client/src/components/brand/Logo.vue
@@ -4,21 +4,13 @@ withDefaults(defineProps<{
 }>(),{
   hideLogoText: false
 });
-const colorMode = useColorMode();
-
-const isDark = computed(() => colorMode.value === 'dark');
 </script>
 
 <template>
   <div class="flex items-center justify-center">
-    <img v-if="isDark"
-         width="100px"
+    <img width="100px"
          class="logo"
          src="~/assets/SyntaxStashLogo-dark.svg"/>
-    <img v-else
-         width="100px"
-         class="logo"
-         src="~/assets/SyntaxStashLogo-light.svg"/>
     <span v-if="!hideLogoText"
           class="logo-text text-4xl font-extrabold text-primary">
       SyntaxStash
