f92fb4fcb26e601744ea2c599b30724143741c82
Dominic Lingham <dominic.lingham@educationcompany.co.uk>
2025-03-05 16:50:53 +0000

    Revert "feat: update prisma schema journal to entry relationship"
    This reverts commit 411eed1e95096e4e0abcc604839c34998694a3e5.

--- client/prisma/migrations/20250305164746_update_journal_to_entry_relationship/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[journalId]` on the table `JournalEntry` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `JournalEntry_journalId_key` ON `JournalEntry`(`journalId`);
