ec3e609ba9f2890a2ea89f63f711229d5371960a
Dominic Lingham <dominic.lingham@educationcompany.co.uk>
2025-03-05 16:27:00 +0000

    feat: complete better auth installation

---
+++ client/lib/auth-client.ts
@@ -0,0 +1,4 @@
+﻿import { createAuthClient } from "better-auth/vue"
+export const authClient = createAuthClient({
+    baseURL: "http://localhost:3000" // the base url of your auth server 
+})
---
+++ client/server/api/[...auth].ts
@@ -0,0 +1,5 @@
+﻿import { auth } from "~/utils/auth"; // import your auth config
+
+export default defineEventHandler((event) => {
+    return auth.handler(toWebRequest(event));
+});
